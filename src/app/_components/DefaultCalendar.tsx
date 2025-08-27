d9613cbb35e20bc57a44a9164aa2c6ce1a77b7dc
eeuneeun <sacroo89@gmail.com>
2025-08-28 01:03:51 +0900

    react-big-calendar 테스트 모듈 작성 완료

---
+++ src/app/_components/DefaultCalendar.tsx
@@ -0,0 +1,19 @@
+"use client";
+
+import { Calendar, momentLocalizer } from "react-big-calendar";
+import moment from "moment";
+
+// Setup the localizer by providing the moment (or globalize, or Luxon) Object
+// to the correct localizer.
+const localizer = momentLocalizer(moment); // or globalizeLocalizer
+
+export const MyCalendar = (props: any) => (
+  <div className="myCustomHeight">
+    <Calendar
+      localizer={localizer}
+      // events={myEventsList}
+      startAccessor="start"
+      endAccessor="end"
+    />
+  </div>
+);
