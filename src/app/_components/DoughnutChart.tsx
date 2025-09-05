da2b960f348c1bdb80ca6dc806409a6a8ae12aef
eeuneeun <sacroo89@gmail.com>
2025-09-05 11:05:10 +0900

    차트 데이터 적용

--- src/app/_components/DoughnutChart.tsx
+++ src/app/_components/DoughnutChart.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Chart as ChartJS, ArcElement, Tooltip, Legend } from "chart.js";
 import { Pie } from "react-chartjs-2";
 import { useLedgerStore } from "../_store/LedgerStore";
@@ -50,10 +50,108 @@ export const data = {
 export default function DoughnutChart() {
   const { startOfMonth, recentList, setRecentList } = useLedgerStore();
 
-  recentList.map((item, idx) => {
-    if (item.category == "식비") {
-    }
+  let food = 0;
+  let entertain = 0;
+  let fare = 0;
+  let telecom = 0;
+  let housing = 0;
+  let insurance = 0;
+  let utilities = 0;
+  let etc = 0;
+
+  const [chartData, setChartData] = useState({
+    labels: [
+      "식비",
+      "여가비",
+      "교통비",
+      "통신비",
+      "주거비",
+      "보험",
+      "공과금",
+      "기타",
+    ],
+    datasets: [
+      {
+        label: "# of Votes",
+        data: [
+          food,
+          entertain,
+          fare,
+          telecom,
+          housing,
+          insurance,
+          utilities,
+          etc,
+        ],
+        backgroundColor: [
+          "#ff638433",
+          "#ff9f4033",
+          "#ffce5633",
+          "#4bc0c033",
+          "#36a2eb33",
+          "#2929fc33",
+          "#9c40ff33",
+          "#9e0cca33",
+        ],
+        borderColor: [
+          "#ff6384ff",
+          "#ff9f40ff",
+          "#ffce56ff",
+          "#4bc0c0ff",
+          "#36a2ebff",
+          "#2d41c3ff",
+          "#9966ffff",
+          "#500ed5ff",
+        ],
+        borderWidth: 1,
+      },
+    ],
   });
 
-  return <Pie data={data} />;
+  async function getChartDataArr() {
+    await recentList.map((item, idx) => {
+      if (item.category == "식비") {
+        food = food + 1;
+      } else if (item.category == "여가비") {
+        entertain = entertain + 1;
+      } else if (item.category == "교통비") {
+        fare = fare + 1;
+      } else if (item.category == "통신비") {
+        telecom = telecom + 1;
+      } else if (item.category == "주거비") {
+        housing = housing + 1;
+      } else if (item.category == "보험") {
+        insurance = insurance + 1;
+      } else if (item.category == "공과금") {
+        utilities = utilities + 1;
+      } else if (item.category == "기타") {
+        etc = etc + 1;
+      }
+    });
+
+    setChartData((prev) => ({
+      ...prev,
+      datasets: [
+        {
+          ...prev.datasets[0], // 기존 dataset 속성 유지
+          data: [
+            food,
+            entertain,
+            fare,
+            telecom,
+            housing,
+            insurance,
+            utilities,
+            etc,
+          ],
+        },
+      ],
+    }));
+  }
+
+  useEffect(() => {
+    getChartDataArr();
+  }, [recentList]);
+
+  return <Pie data={chartData} />;
 }
